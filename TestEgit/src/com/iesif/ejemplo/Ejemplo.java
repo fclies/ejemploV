86b09010512fbd9bf69f88e137a7425e704a1d7c
FCL <fclies@gmail.com>
2019-01-22 16:38:50 +0100

    Versión inicial: Sólo mostraremos los datos de entrada por pantalla

---
+++ TestEgit/src/com/iesif/ejemplo/Ejemplo.java
@@ -0,0 +1,16 @@
+package com.iesif.ejemplo;
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		int n=4;
+		double a=5.2;
+		char c= 'n';
+		
+		System.out.println("N = " + n);
+		System.out.println("A = " + a);
+		System.out.println("C = " + c);
+
+	}
+
+}
